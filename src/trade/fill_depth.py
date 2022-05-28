fc3302e9ed7b654a5a06db4cbc192d2c084ebb06
HU-MINI <zenithofyou@gmail.com>
2022-05-29 00:01:10 +0800

    update fill_depth

--- src/trade/fill_depth.py
+++ src/trade/fill_depth.py
@@ -20,9 +20,16 @@ async def fill_depth(hot_coin, websocket):
             print_prefix = f'[Fill Depth: {self_cnt}]'
             config.load_config()
 
+            # Check fill_depth_on
+            if not config.fill_depth_on:
+                logger.warning('fill_depth 关闭, 10秒后重试')
+                self_cnt = 0
+                time.sleep(10)
+                continue
+
             # Get Depth
             self_coin_depth_data = hot_coin.get_depth()
-            logger.info(self_coin_depth_data)
+            # logger.info(self_coin_depth_data)
 
             sellprice, buyprice = [], []
             if 'data' in self_coin_depth_data and 'depth' in self_coin_depth_data['data']:
@@ -35,28 +42,22 @@ async def fill_depth(hot_coin, websocket):
             else:
                 logger.warning(f'{print_prefix} 深度获取失败')
                 continue
-            print(sellprice)
-            print(buyprice)
             trade_all_list = []
             self_coin_b1_price = Decimal(round(buyprice[0], config.price_decimal_num))
-            print('self_coin_b1_price', self_coin_b1_price)
             price_step = round(Decimal(0.1) ** config.price_decimal_num, config.price_decimal_num)
-            print('price_step', price_step)
             for i in range(20):
                 fill_sell_price = self_coin_b1_price + price_step * (i + 1)
                 push_sell_price = round(fill_sell_price, config.price_decimal_num)
-                push_sell_amount = round(random.uniform(config.fork_trade_random_amount_min,
-                                                        config.fork_trade_random_amount_max), config.vol_decimal_num)
+                push_sell_amount = round(random.uniform(config.fill_depth_random_amount_min,
+                                                        config.fill_depth_random_amount_max), config.vol_decimal_num)
                 if fill_sell_price not in sellprice:
-                    print(f'push_sell_price {push_sell_price} , amount: {push_sell_amount}, trade_type: 0')
                     trade_all_list.append({'price': push_sell_price, 'amount': push_sell_amount, 'trade_type': 0})
 
                 fill_buy_price = self_coin_b1_price - price_step * (i + 1)
                 push_buy_price = round(fill_buy_price, config.price_decimal_num)
-                push_buy_amount = round(random.uniform(config.fork_trade_random_amount_min,
-                                                       config.fork_trade_random_amount_max), config.vol_decimal_num)
+                push_buy_amount = round(random.uniform(config.fill_depth_random_amount_min,
+                                                       config.fill_depth_random_amount_max), config.vol_decimal_num)
                 if fill_buy_price not in buyprice:
-                    print(f'push_buy_price {push_buy_price} , amount: {push_buy_amount}, trade_type: 0')
                     trade_all_list.append({'price': push_buy_price, 'amount': push_buy_amount, 'trade_type': 1})
 
             # 校验限制
@@ -65,11 +66,6 @@ async def fill_depth(hot_coin, websocket):
                 if not config.ALERT_PRICE_MAX > item["price"] > config.ALERT_PRICE_MIN:
                     logger.warning(f'{print_prefix}交易价格超出预警区间, 价格: {item["price"]}')
                     remind_tg(config.ALERT_PRICE_TG_CHAT, f'{print_prefix}交易价格超出预警区间, 价格: {item["price"]}')
-                    check_error_flag = True
-                    break
-                if item["amount"] > config.fork_trade_amount_max:
-                    logger.warning(f'{print_prefix}交易量超额, 交易量：{item["amount"]}')
-                    remind_tg(config.ALERT_PRICE_TG_CHAT, f'{print_prefix}交易量超额, 交易量：{item["amount"]}')
                     check_error_flag = True
                     break
             if check_error_flag:
@@ -85,8 +81,8 @@ async def fill_depth(hot_coin, websocket):
                 resultId = result['data']['ID']
                 logger.info(f'{print_prefix} 下单成功， ID：{resultId}')
             # Sleep
-            logger.info(f'{print_prefix} 交易完成, {config.fork_trade_interval}秒后重新进入')
-            time.sleep(config.fork_trade_interval)
+            logger.info(f'{print_prefix} 交易完成, {config.fill_depth_interval}秒后重新进入')
+            time.sleep(config.fill_depth_interval)
             self_cnt += 1
 
     except Exception as e:
